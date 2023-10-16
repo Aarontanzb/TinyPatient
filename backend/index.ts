1fba5a6ae3b3bfda65ac7cbf1e76484360ff5a3b
Aaron Tan <aarontanzb@gmail.com>
2023-10-16 16:54:15 +0800

    added mongodb

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import patientsRouter from './routes/patients';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -14,13 +17,13 @@ app.use(express.json());
 
 const PORT = 3001;
 
-app.get('/api', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
-});
-
 app.use('/api/patients', patientsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+mongoose.connect(process.env.MONGO_URI as string)
+  .then(() => {
+    app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} and connected to MongoDB`);
 });
+  }).catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
