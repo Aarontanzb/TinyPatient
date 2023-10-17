5fb2342292d386f1a07679987553aa1abc2d5634
Aaron Tan <aarontanzb@gmail.com>
2023-10-17 19:06:32 +0800

    Fix cypress bug

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "cypress";
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 export default defineConfig({
-  projectId: "56nq15",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
